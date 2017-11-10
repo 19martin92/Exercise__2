adaedbeec9e689e7c3abb8ff489cce4951613f1e
MARTIN\marti_000 <martin@ehrmann-netz.de>
2017-11-10 22:24:26 +0100

    smooth curves

--- MarchingCubes.h
+++ MarchingCubes.h
@@ -342,7 +342,11 @@ Vector3d VertexInterp(double isolevel, const Vector3d& p1, const Vector3d& p2, d
 	// x
 	// f(p1) = valp1
 
-	return (p1+p2)/2; // replace me
+	double factor = (isolevel - valp1) / (valp2 - valp1);
+
+	return p1 + (p2 - p1)*factor;
+
+	//return (p1+p2)/2; // replace me
 }
 
 /*
